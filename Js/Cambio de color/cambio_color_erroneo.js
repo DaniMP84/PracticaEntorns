6db40dc023846249605a55dda22920e9fa286a6f
DaniMP84 <xDani84x@gmail.com>
2024-12-02 19:57:37 +0100

    Commit de proba 2

--- Js/Cambio de color/cambio_color_erroneo.js
+++ Js/Cambio de color/cambio_color_erroneo.js
@@ -1,5 +1,5 @@
 function cambiarColor() {
     const colores = ["red", "blue", "green", "yellow", "purple"];
     const indice = Math.random() * colores.length; 
-    document.body.style.backgroundColor = colores[indice]
+    document.body.style.backgroundColors = colores[indice]
 }
